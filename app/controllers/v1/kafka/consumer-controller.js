8a6859ec7fe0f802510f2d8e7eea018d0cffa74e
SergeyDzIntapp <sergey.dzyuban@gmail.com>
2016-04-06 13:20:21 +0300

    removed broken reference to kafka

--- app/controllers/v1/kafka/consumer-controller.js
+++ app/controllers/v1/kafka/consumer-controller.js
@@ -1,5 +1,3 @@
-var kafka = require('kafka')
-
 function ConsumerController() {
 }
 
--- app/controllers/v1/kafka/producer-controller.js
+++ app/controllers/v1/kafka/producer-controller.js
@@ -2,6 +2,12 @@ function ProducerController() {
 }
 
 function post(req, res, next) {
+    var messages = new Array();
+    
+    for (i = 0; i < 110000; i++) {
+        messages.push({ name: "Client #" + i, Code: i, Description: "test kafka record"});
+    }
+
     var kafka = require('kafka-node'),
         Producer = kafka.Producer,
         KeyedMessage = kafka.KeyedMessage,
@@ -9,16 +15,16 @@ function post(req, res, next) {
         producer = new Producer(client),
         km = new KeyedMessage('key', 'message'),
         payloads = [
-            { topic: 't0', messages: 'hello world', partition: 0 }
+            { topic: 't0', messages: messages, partition: 0 }
         ];
-    producer.on('ready', function () {
-        producer.send(payloads, function (err, data) {
+    producer.on('ready', function() {
+        producer.send(payloads, function(err, data) {
             console.log(data);
             res.status(200).json({ success: data });
         });
     });
 
-    producer.on('error', function (err) { res.status(500).json({ error: err }); })
+    producer.on('error', function(err) { res.status(500).json({ error: err }); })
 }
 
 ProducerController.prototype = {
